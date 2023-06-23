e7e360cc5c01511ad49a0a8375cff0a6ba271b3e
Mylena <myvendramini@gmail.com>
2023-06-23 19:30:49 +0100

    feat: create server and add scripts package.json

---
+++ server/controllers/controller.ts
@@ -0,0 +1,22 @@
+// import express, { Express, Request, Response } from 'express';
+
+// async function createDoctor(req, res) {
+//   try {
+//   } catch (error) {}
+// }
+// async function getDoctor(req, res) {}
+// async function getDoctors(req, res) {}
+// async function createJuniorDoctor(req, res) {}
+// async function getJuniorDoctor(req, res) {}
+// async function createPatient(req, res) {}
+// async function getPatient(req, res) {}
+// async function getPatients(req, res) {}
+// async function updatePatient(req, res) {}
+// async function deletePatient(req, res) {}
+// async function sendMessage(req, res) {}
+// async function getMessages(req, res) {}
+// async function createPrescription(req, res) {}
+// async function createDoctorNote(req, res) {}
+// async function createJuniorNote(req, res) {}
+// async function createPatientSummary(req, res) {}
+// async function getLastCheckup(req, res) {}
---
+++ server/index.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
