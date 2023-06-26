72e7c0b694c0f614607c2d0bca78b1227f9ad579
Ateja <atejukas123@gmail.com>
2023-06-26 18:01:54 +0100

    feat: added grid on dashboard

--- client/app/patient/(dashboard)/page.tsx
+++ client/app/patient/(dashboard)/page.tsx
@@ -2,17 +2,27 @@ import './dashboard.css'
 import Profile from './profile';
 import Appoitments from './appointments';
 import CheckUp from './last-checkup'
+import Prescriptions from './prescriptions'
 // import Cal from './calendar'
 
 export default function Patient() {
   return (
-    <main>
+    <main className="grid-container">
+    <div className="profile-box">
     <Profile/>
-    <div className='appoitments-box'>
+    </div>
+    <div className='last-checkup-box'>
     <CheckUp/>
     </div>
+    <div>
+    <div className="prescriptions-box">
+    <Prescriptions/>
+    </div>
+    </div>
     {/* <Cal cellRender={cellRender} /> */}
+    <div className='appoitment-box'>
     <Appoitments/>
+    </div>
     </main>
   );
 }
---
+++ client/app/patient/(dashboard)/prescriptions.tsx
@@ -0,0 +1,14 @@
+import "./prescriptions.css";
+
+
+export default function Prescriptions() {
+  return (
+    <main>
+      <div className="prescriptions-container">
+       <h1>MEDICATION</h1>
+      </div>
+    </main>
+  );
+}
+
+
