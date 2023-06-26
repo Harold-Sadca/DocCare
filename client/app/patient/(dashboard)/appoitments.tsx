6c4094cbbe2fe25b252151edd71c5a2fdf36c70d
Ateja <atejukas123@gmail.com>
2023-06-26 20:06:10 +0100

    feat: more margin

--- client/app/patient/(dashboard)/appoitments.tsx
+++ client/app/patient/(dashboard)/appoitments.tsx
@@ -8,10 +8,12 @@ export default function Appoitments() {
       <h1>Upcoming Appoitments:</h1>
       <div className="appoitment-list">
        <div className="appoitment-list-container">
-       <div>
+       <div className="each-appoitment">
         <img src='../../../public/checkup-emoji.png'></img>
+        <div className="each-appoitment-text">
         <h2>Check up</h2>
         <p>Aug 18</p>
+        </div>
        </div>
        </div>
       </div>
--- client/app/patient/(dashboard)/doctor-list.tsx
+++ client/app/patient/(dashboard)/doctor-list.tsx
@@ -9,7 +9,7 @@ export default function DoctorList() {
       <div className="doctor-list">
        <div className="each-doctor">
         <img src="https://images.pexels.com/photos/4270088/pexels-photo-4270088.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
-       <div>
+       <div className="each-doctor-name">
         <h2>Michaela Hans</h2>
         <p>Dentist</p>
         </div>
