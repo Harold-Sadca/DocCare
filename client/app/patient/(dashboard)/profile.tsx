0b0558f4d909718286d0e30a0179ef7a1c74dc34
Ateja <atejukas123@gmail.com>
2023-06-26 15:35:11 +0100

    feat: layout for profile

--- client/app/patient/(dashboard)/profile.tsx
+++ client/app/patient/(dashboard)/profile.tsx
@@ -1,20 +1,26 @@
-import './profile.css'
-
-
-
+import "./profile.css";
 
 export default function Profile() {
-  return(
+  return (
     <main>
-      <div className='profile'>
-        <div className='about-patient'>
-          <div className='profile-pic'>
-          <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
+      <div className="profile">
+        <div className="about-patient">
+          <div className="profile-pic">
+            <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
           </div>
-        <h1>Michaela Hans</h1>
-        <p>36 years old, Female</p>
-      </div>
+          <h1>Michaela Hans</h1>
+          <p>36 years old, Female</p>
+          </div>
+          <div className="general-info">
+            <h2>General info:</h2>
+            <p>Date of birth: 1985, January 13</p>
+            <p>Female</p>
+          </div>
+          <div className="ilnesses">
+          <h3>Illnesses:</h3>
+          <p>Anxiety</p>
+          </div>
       </div>
     </main>
-  )
+  );
 }
