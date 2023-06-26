e6f7d77d62e7b72cf1cb75cc5315fff21f7bf9c2
Mylena <myvendramini@gmail.com>
2023-06-26 14:43:09 +0100

    feat: register form

--- client/app/doctor/register/page.tsx
+++ client/app/doctor/register/page.tsx
@@ -10,7 +10,10 @@ import {
   Select,
   Switch,
   TreeSelect,
+  Upload,
 } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+const { TextArea } = Input;
 import React, { useState } from 'react';
 
 import Navbar from './navbar';
@@ -29,99 +32,115 @@ export default function Register() {
   return (
     <>
       <Navbar />
-      <div className='flex min-h-screen flex-col'>
-        <div className='grid grid-cols-2 gap-4 h-screen'>
-          <div className='flex flex-col items-center justify-evenly'>
-            <div className='flex flex-row items-start justify-start'>
-              <img
-                className='h-auto w-44 rounded'
-                src='/doctor-mobile.png'
-                alt='Your Company'
-              />
-            </div>
-          </div>
-          <div className='flex flex-col items-center justify-center'>
-            <h2 className='font-bold text-2xl text-primary'>Register</h2>
-            <h3>Explore the future with us.</h3>
-            <Form
-              labelCol={{ span: 4 }}
-              wrapperCol={{ span: 14 }}
-              layout='horizontal'
-              initialValues={{ size: componentSize }}
-              onValuesChange={onFormLayoutChange}
-              size={componentSize as SizeType}
-              style={{ maxWidth: 600 }}
-              action='/'
-              method='post'
+      <div className='flex min-h-screen flex-col items-center justify-center'>
+        <h2 className='font-bold text-2xl text-primary'>Register</h2>
+        <h3>Explore the future with us.</h3>
+        <div className='flex min-h-screen flex-col items-center justify-center'>
+          <Form
+            labelCol={{ span: 6 }}
+            wrapperCol={{ span: 14 }}
+            layout='horizontal'
+            initialValues={{ size: componentSize }}
+            onValuesChange={onFormLayoutChange}
+            size={componentSize as SizeType}
+            style={{ maxWidth: 900 }}
+            action='/'
+            method='post'
+          >
+            <Form.Item label='Name' htmlFor='name'>
+              <Input type='text' id='name' name='name' required />
+            </Form.Item>
+            <Form.Item label='Email' htmlFor='email'>
+              <Input type='email' id='email' name='email' required />
+            </Form.Item>
+            <Form.Item label='Password' htmlFor='password'>
+              <Input type='password' id='password' name='password' required />
+            </Form.Item>
+            <Form.Item label='Phone Number' htmlFor='phone'>
+              <Input type='tel' id='phone' name='phone' required />
+            </Form.Item>
+            <Form.Item label='Address' htmlFor='address'>
+              <Input type='text' id='address' name='address' required />
+            </Form.Item>
+            <Form.Item label='License Number' htmlFor='license'>
+              <Input type='text' id='license' name='license' required />
+            </Form.Item>
+            <Form.Item label='Gender' htmlFor='gender'>
+              <Radio.Group id='gender' name='gender'>
+                <Radio id='male' value='male'>
+                  Male
+                </Radio>
+                <Radio id='female' value='female'>
+                  Female
+                </Radio>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item label='About' htmlFor='about'>
+              <TextArea rows={4} id='about' name='about' required />
+            </Form.Item>
+            <Form.Item label='Profile Picture' htmlFor='profile'>
+              <Upload action='/upload.do' listType='picture-card'>
+                <div>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </div>
+              </Upload>
+            </Form.Item>
+            <Form.Item label='Specialisation' htmlFor='specialisation'>
+              <Radio.Group id='specialisation' name='specialisation'>
+                <Radio id='specialisation1' value='General Practice'>
+                  General Practice
+                </Radio>
+                <Radio id='specialisation2' value='Internal Medicine'>
+                  Internal Medicine
+                </Radio>
+                <Radio id='specialisation3' value='Pediatrics'>
+                  Pediatrics
+                </Radio>
+                <Radio id='specialisation4' value='Obstetrics and Gynecology'>
+                  Obstetrics and Gynecology
+                </Radio>
+                <Radio id='specialisation5' value='Surgery'>
+                  Surgery
+                </Radio>
+                <Radio id='specialisation6' value='Psychiatry'>
+                  Psychiatry
+                </Radio>
+                <Radio id='specialisation7' value='Dermatology'>
+                  Dermatology
+                </Radio>
+                <Radio id='specialisation8' value='Ophthalmology'>
+                  Ophthalmology
+                </Radio>
+                <Radio id='specialisation9' value='Ear Nose and Throat (ENT)'>
+                  Ear Nose and Throat (ENT)
+                </Radio>
+                <Radio id='specialisation10' value='Cardiology'>
+                  Cardiology
+                </Radio>
+                <Radio id='specialisation11' value='Endocrinology'>
+                  Endocrinology
+                </Radio>
+                <Radio id='specialisation12' value='Gastroenterology'>
+                  Gastroenterology
+                </Radio>
+                <Radio id='specialisation13' value='Neurology'>
+                  Neurology
+                </Radio>
+                <Radio id='specialisation14' value='Oncology'>
+                  Oncology
+                </Radio>
+              </Radio.Group>
+            </Form.Item>
+            <button
+              className='bg-tertiary hover:bg-tertiary-dark text-white font-bold py-2 px-4 m-2 rounded'
+              type='submit'
             >
-              <Form.Item label='Name' htmlFor='name'>
-                <Input type='text' id='name' name='name' required />
-              </Form.Item>
-              <Form.Item label='Email' htmlFor='email'>
-                <Input type='email' id='email' name='email' required />
-              </Form.Item>
-
-              <label htmlFor='password'>Password:</label>
-              <input type='password' id='password' name='password' required />
-              <label htmlFor='phone'>Phone Number:</label>
-              <input type='tel' id='phone' name='phone' required />
-              <label htmlFor='address'>Address:</label>
-              <input type='text' id='address' name='address' required />
-              <label htmlFor='license'>License Number:</label>
-              <input type='text' id='license' name='license' required />
-              <label htmlFor='gender'>Gender:</label>
-              <div>
-                <input type='radio' id='male' name='gender' value='male' />
-                <label htmlFor='male'>Male</label>
-                <input type='radio' id='female' name='gender' value='female' />
-                <label htmlFor='female'>Female</label>
-              </div>
-              <label htmlFor='about'>About:</label>
-              <textarea id='about' name='about' required></textarea>
-              <label htmlFor='profile'>Profile Picture:</label>
-              <input
-                type='file'
-                id='profile'
-                name='profile'
-                accept='image/*'
-                required
-              />
-              <label htmlFor='specialisation'>Specialisation:</label>
-              <div>
-                <input
-                  type='radio'
-                  id='specialisation1'
-                  name='specialisation'
-                  value='specialisation1'
-                />
-                <label htmlFor='specialisation1'>Specialisation 1</label>
-                <input
-                  type='radio'
-                  id='specialisation2'
-                  name='specialisation'
-                  value='specialisation2'
-                />
-                <label htmlFor='specialisation2'>Specialisation 2</label>
-                <input
-                  type='radio'
-                  id='specialisation3'
-                  name='specialisation'
-                  value='specialisation3'
-                />
-                <label htmlFor='specialisation3'>Specialisation 3</label>
-              </div>
-
-              <button
-                className='bg-tertiary hover:bg-tertiary-dark text-white font-bold py-2 px-4 m-2 rounded'
-                type='submit'
-              >
-                Register
-              </button>
-            </Form>
-          </div>
+              Register
+            </button>
+          </Form>
         </div>
       </div>
-
       <Footer />
     </>
   );
