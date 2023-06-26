58bf7234897af4459b3d091fd1d434c95cb9b9dc
Ateja <atejukas123@gmail.com>
2023-06-26 16:04:23 +0100

    feat: profile done

--- client/app/patient/(dashboard)/profile.tsx
+++ client/app/patient/(dashboard)/profile.tsx
@@ -9,7 +9,7 @@ export default function Profile() {
             <img src="https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"></img>
           </div>
           <h1>Michaela Hans</h1>
-          <p>36 years old, Female</p>
+          <p>36 years old</p>
           </div>
           <div className="general-info">
             <h2>General info:</h2>
@@ -18,7 +18,25 @@ export default function Profile() {
           </div>
           <div className="ilnesses">
           <h3>Illnesses:</h3>
-          <p>Anxiety</p>
+          <div className="each-illness">
+            <div>
+            Anxiety
+            </div>
+            <div>
+            Depression
+            </div>
+          </div>
+          <div className="checkup">
+           <h4>Last Checkup:</h4>
+           <div className="checkup-container">
+           <p className="date">19/07/2022</p>
+           <div className="doctor-notes">
+           <p>Doctors Notes:</p>
+           <p>Yoga and streches once a week</p>
+           </div>
+           </div>
+          </div>
+
           </div>
       </div>
     </main>
