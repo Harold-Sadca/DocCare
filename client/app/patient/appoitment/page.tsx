75cc0ab0607a646d67ae71636a7432d7f8bcaae0
Ateja <atejukas123@gmail.com>
2023-06-28 12:05:19 +0100

    feat: icons for appoitment

--- client/app/patient/appoitment/page.tsx
+++ client/app/patient/appoitment/page.tsx
@@ -1,12 +1,15 @@
 import AuthNavbar from '@/app/(components)/auth-navbar';
-
+import "./appoitment-dashboard.css"
 
 export default function PatientAppoitment() {
     return (
       <main className='patient-appoitment-main'>
-         {/* <AuthNavbar user={'patient'} auth={'login'} /> */}
+         <AuthNavbar user={'patient'} auth={'login'} />
          {/* change the navbar to the correct info  */}
-        <h1>Patient Appoitment!</h1>
+        <h1>Make an Appoitment</h1>
+        <img src='/1.png' className='icon'/>
+        <img src='/2.png' className='icon'/>
+        <img src='/3.png' className='icon'/>
       </main>
     );
   }
