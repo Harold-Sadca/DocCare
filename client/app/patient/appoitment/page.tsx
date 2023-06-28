85beab9c170c34434eb252b2e5420ac417927544
Ateja <atejukas123@gmail.com>
2023-06-28 11:24:22 +0100

    feat: connected to db

---
+++ client/app/patient/appoitment/page.tsx
@@ -0,0 +1,13 @@
+import AuthNavbar from '@/app/(components)/auth-navbar';
+
+
+export default function PatientAppoitment() {
+    return (
+      <main className='patient-appoitment-main'>
+         {/* <AuthNavbar user={'patient'} auth={'login'} /> */}
+         {/* change the navbar to the correct info  */}
+        <h1>Patient Appoitment!</h1>
+      </main>
+    );
+  }
+  
