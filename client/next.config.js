7e87be8bb58eb6ba398f06cf15c15cce1d7a7c33
Ateja <atejukas123@gmail.com>
2023-06-30 10:55:39 +0100

    Feat: next config file for Images

--- client/next.config.js
+++ client/next.config.js
@@ -1,14 +1,13 @@
-{import('next').NextConfig} 
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          // hostname: '**',
-          hostname: 'images.pexels.com'
-        },
-      ],
-    },
-  }
-  module.exports = nextConfig
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        // hostname: '**',
+        hostname: 'images.pexels.com'
+      },
+    ],
+  },
+}
+module.exports = nextConfig
