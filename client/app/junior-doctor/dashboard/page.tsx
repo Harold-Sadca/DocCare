aaa3adf0e7e40128e1c2339321f28fde44fa172d
Ateja <atejukas123@gmail.com>
2023-06-30 18:05:38 +0100

    fix: now can have the chat in same page with JuniorDoctorMessages

--- client/app/junior-doctor/dashboard/page.tsx
+++ client/app/junior-doctor/dashboard/page.tsx
@@ -1,22 +1,25 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
+import JuniorDoctorMessages from './messages';
 import '.././junior-doctor.css';
 import apiService from '@/services/APIservices';
 import { useEffect, useState } from 'react';
 import { TypePatient } from '../../../../server/types/types';
 import AllPatients from './patients';
 import AuthNavbar from '@/app/(components)/auth-navbar';
-import  JuniorDoctorMessages from './messages'
+import { useAppSelector } from '@/redux/store';
 import { TUser } from '@/types/types';
+
 
 export default function JuniorDoctorDashBoard() {
   const [allPatients, setAllPatients] = useState<TypePatient[]>([]);
-
+  const currentJunior = useAppSelector(
+    (state) => state.currentJuniorReducer.value
+  );
   async function getPatients(token: string) {
     const patients = await apiService.getAllPatients(token);
     setAllPatients(patients as TypePatient[]);
   }
-
   useEffect(() => {
     const token =
       typeof window !== 'undefined' &&
@@ -28,15 +31,20 @@ export default function JuniorDoctorDashBoard() {
       getPatients(token);
     }
   }, []);
-
   return (
     <main>
       <AuthNavbar user={'junior-doctor'} auth={'login'} />
-    
       <AllPatients allPatients={allPatients} />
       <div className='chat-box'>
-        {/* <JuniorDoctorMessages currentJunior={junior as TUser} /> */}
+        <JuniorDoctorMessages currentJunior={currentJunior as TUser} />
       </div>
     </main>
   );
 }
+
+
+
+
+
+
+
--- client/redux/store.ts
+++ client/redux/store.ts
@@ -3,9 +3,9 @@ import authReducer from './features/auth-slice';
 import currentPatientReducer from './features/patient-slice';
 import currentDoctorReducer from './features/doctor-slice';
 import chatPatientReducer from './features/chat-patient-slice';
+import currentJuniorReducer from './features/junior-slice';
 import AvailableSpecialist from './features/available-doctors-slice';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
-
 export const store = configureStore({
   reducer: {
     authReducer,
@@ -13,11 +13,10 @@ export const store = configureStore({
     currentDoctorReducer,
     chatPatientReducer,
     AvailableSpecialist,
+    currentJuniorReducer
   },
 });
-
 // type of store:
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
