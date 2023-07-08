35651d918ad71e558ffe204804361ba440fae0a0
Harold Sadca <sadcah@yahoo.com>
2023-07-08 09:04:25 +0100

    Fix:Cleaned Server File

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,6 @@ import { createServer } from 'http';
 import { Server } from 'socket.io';
 import logger from './logger';
 import dotenv from 'dotenv';
-import { router } from './routers';
 dotenv.config();
 
 const corsConfig = {
@@ -32,29 +31,17 @@ app.use(patientRouter);
 app.use(messagesRouter);
 app.use(juniorDoctorRouter);
 app.use(doctorRouter);
-// app.use(router);
 
 io.use((socket, next) => {
-  // here we check if there is a name in the socket
-  // we assigned this property in the frontend
   const name = socket.handshake.auth.name;
   if (!name) {
-    // if there isnt a name we throw an error and refuse the connection
     return next(new Error('invalid username'));
   }
-  // if we manage to get here it means they have been authenticated
-  // so we call next to go to the next middleware
   next();
 });
 
 io.on('connection', (socket) => {
   socket.broadcast.emit('patient logged');
-
-  // socket.to('junior').emit('patient logged')
-
-  // 'socket.join(room name)' would let us join the private room
-  // but if its not there it will create it so in this case
-  // we are using it to create a private room
   const newRoom = socket.handshake.auth.name;
   if (newRoom === 'junior') {
     socket.join('junior');
@@ -62,29 +49,17 @@ io.on('connection', (socket) => {
     socket.join(newRoom);
   }
 
-  // here we check the events that are coming in from the frontend
-  // we have created this 'from junior' from frontend
-  // so this means when you detect an event called 'from junior' do the following
   socket.on('from junior', async (message, receiver) => {
-    //this saves it to the database
     const newMessage = await sendMessageModel(message);
-    logger.warn(newMessage);
-    // socket.to(room name) would send a message only to that room
-    // to all clients on the socket in room 'receiver' except the sender
     socket.to(receiver).emit('from junior', newMessage);
-    // socket.to('junior').emit('junior sent', newMessage)
   });
   socket.on('patient message', async (message) => {
     const newMessage = await sendMessageModel(message);
-    logger.warn(newMessage);
     socket.to('junior').emit('patient message', newMessage);
-    // socket.to(message.sender_name).emit('patient sent', newMessage)
     socket.broadcast.emit('patient logged');
   });
-  logger.warn(newRoom);
 
   socket.on('patient logged', (args) => {
-    logger.info('logged', args);
     socket.broadcast.emit('patient logged');
   });
 });
